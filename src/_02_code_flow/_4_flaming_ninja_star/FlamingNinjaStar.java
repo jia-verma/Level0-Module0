e514e280691641ccbfb850c90555cdd242c70428
jia-verma <jiaverma28@gmail.com>
2025-04-19 01:29:46 +0000

    if(name.equalsIgnoreCase("Bob")){

--- src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
+++ src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
@@ -10,36 +10,52 @@ public class FlamingNinjaStar {
 		int flameSize = 130;		//the length of the flaming arms
 		
 		// Make a new robot, and set it's pen down.
+		Robot rob = new Robot();
+		rob.penDown();
 
 		// Set the robot speed to 100
-		
+		rob.setSpeed(100);
 		// Set the robot window size to 800 x 800
+		rob.setWindowSize(800,800);
 
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
+		int count = 0;
 		
 	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
+		   while ( count < 25) {
 
 			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
+			   rob.turn(360/8);
 		
 			   // MOVE           Move the robot 64 pixels
+			   rob.move(64);
 
 			   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
+			   rob.turn(-40);
+			   rob.setPenColor(Color.ORANGE);
 			
 			   // DRAW FLAME     Move the robot the distance in the variable flameSize
+			   rob.move(flameSize);
 			
 				//               Turn the robot 170 degrees
+				rob.turn(170);
 			
 				//               Move the robot the distance in the variable flameSize (again)
+				rob.move(flameSize);
+				rob.setPenColor(Color.BLUE);
 			
 			   // TURN RIGHT     Turn the robot 64 degrees to the right
+			   rob.turn(64);
 			
 				// MOVE         Move the robot the distance in the variable baseSize
+				rob.move(baseSize);
 		
 				//  INCREASE COUNT. Increase the count by 1
+				count += 1;
 		
 		//  End the while loop here
-		
+		   }
 		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
 		//        This is one arm of the ninja star.
 		
--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,6 +8,7 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
+		String knowledge = JOptionPane.showInputDialog("Do you know how to code?");
 
 		// 2. If they say "yes", tell them they will rule the world.
 

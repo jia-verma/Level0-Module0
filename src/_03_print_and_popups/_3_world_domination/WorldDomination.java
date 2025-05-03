7dbd78e35de30534a747f65bd52324840d222493
jia-verma <jiaverma28@gmail.com>
2025-05-03 01:30:26 +0000

    Start at AwesomeOrNot

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -1,6 +1,8 @@
 package _03_print_and_popups._3_world_domination;
 
 
+import java.sql.JDBCType;
+
 import javax.swing.JOptionPane;
 
 public class WorldDomination {
@@ -11,6 +13,13 @@ public class WorldDomination {
 		String knowledge = JOptionPane.showInputDialog("Do you know how to code?");
 
 		// 2. If they say "yes", tell them they will rule the world.
+		if (knowledge.equals("Yes")) {
+			JOptionPane.showMessageDialog(null, "You will rule the world!");
+		}else{
+			JOptionPane.showMessageDialog(null, "Sign up for classes at The League!");
+		}
+	
+
 
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,16 +1,28 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
-		// Create a variable to hold the user's score. Set it equal to zero. 
+		// Create a variable to hold the user's score. Set it equal to zero.
+		int score = 0; 
+
 		
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+				String quiz = JOptionPane.showInputDialog("What are you looking forward to?");
 				// 3.  Use an if statement to check if their answer is correct
-				
+				if (quiz.equals("Agatha Christie Club")) {
+					score +=1;
+				}
+				String question = JOptionPane.showInputDialog("What are you looking backward to?");
+				if (question.equals("Orchestra")) {
+					score +=1;
+				}
+				JOptionPane.showMessageDialog(null, "Your score is "+score);
+			
 				// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
